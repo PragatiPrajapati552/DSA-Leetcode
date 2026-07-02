ae46c8337caefbf65e04a8d8f1a6c60736c2475c
PRAGATI PRAJAPATI <pragatiprajapati552@gmail.com>
2026-07-02 23:47:09 +0530

    Time: 0 ms (100%), Space: 43.2 MB (25.85%) - LeetHub

--- 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -1,41 +1,33 @@
 class Solution {
     public boolean check(int[] nums) {
-
-        int n = nums.length;
-        int[] arr = new int[n];
-
+        int arr[] =  new int[nums.length];
         int idx = -1;
-
-        // Find rotation point
-        for(int i = 0; i < n - 1; i++) {
-            if(nums[i] > nums[i + 1]) {
-                idx = i + 1;
-                break;
-            }
+        for(int i =0; i<nums.length-1;i++){
+            if(nums[i] > nums[i+1]){
+            idx = i+1;
+            break;
+            }  
         }
 
-        // Already sorted
         if(idx == -1)
-            return true;
+        return true;
 
-        int start = idx;
         int id = 0;
-
-        // Rotate the array
-        for(int i = 0; i < n; i++) {
-            if(start < n) {
-                arr[i] = nums[start++];
-            } else {
-                arr[i] = nums[id++];
+        for(int i=0;i<nums.length;i++){
+            if(idx != nums.length){
+            arr[i] = nums[idx];
+            idx++;
             }
+            else {
+                arr[i] = nums[id];
+                id++;
+            }   
         }
-
-        // Check if rotated array is sorted
-        for(int i = 0; i < n - 1; i++) {
-            if(arr[i] > arr[i + 1])
-                return false;
+        
+        for(int i=0; i<nums.length-1;i++){
+            if(arr[i] > arr[i+1])
+            return false;    
         }
-
         return true;
     }
 }
