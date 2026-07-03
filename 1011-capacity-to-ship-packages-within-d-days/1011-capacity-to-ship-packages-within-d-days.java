62b8d78ba52547934fc8ef77a1eb4bb5c22879f2
PRAGATI PRAJAPATI <pragatiprajapati552@gmail.com>
2026-07-04 00:10:10 +0530

    Time: 11 ms (68.45%), Space: 50.5 MB (6.34%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.java
@@ -1,45 +1,42 @@
 class Solution {
     public int shipWithinDays(int[] weights, int days) {
-
-        int sum = 0;
-        int maxWeight = 0;
-
-        for(int w : weights){
-            sum += w;
-            maxWeight = Math.max(maxWeight, w);
+        int sum=0;
+        for(int i=0; i<weights.length; i++){
+            sum += weights[i];
         }
-
-        int min = maxWeight;
+        int maxm = Integer.MIN_VALUE;
+        for(int i=0;i<weights.length;i++){
+            maxm = Math.max(maxm, weights[i]);
+        }
+        int min = maxm;
         int max = sum;
-        int ans = sum;
-
+        int ans = 0;
         while(min <= max){
-
-            int mid = min + (max - min)/2;
-
-            int day = 1;
-            int load = 0;
-
-            for(int w : weights){
-
-                if(load + w <= mid){
-                    load += w;
-                }else{
+            int mid = (min + max)/2;
+            int day = 0;
+            int s=0;
+            for(int i = 0; i<weights.length;i++){
+                if((s + weights[i]) <= mid)
+                s += weights[i];
+                else{
                     day++;
-                    load = w;
+                    s=0;
+                    i--;
                 }
-
             }
+            if(s<=mid)
+            day++;
 
-            if(day <= days){
+            if(day <= days){        //cause in que its asked within not exact days.
                 ans = mid;
-                max = mid - 1;
-            }else{
-                min = mid + 1;
+                max = mid-1;
             }
-
+            else if(day < days)
+            max = mid-1;
+            else
+            min = mid+1;
         }
-
         return ans;
+
     }
 }
