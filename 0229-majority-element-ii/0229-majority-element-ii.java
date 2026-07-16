2c9949e8bc435f8895e23fcedc889f8d69a5a0fc
PRAGATI PRAJAPATI <pragatiprajapati552@gmail.com>
2026-07-16 20:48:57 +0530

    Time: 2 ms (99.95%), Space: 52.5 MB (52.88%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,44 +1,42 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        int candidate1 = 0, candidate2 = 0;
-        int count1 = 0, count2 = 0;
-
-        // Find potential candidates
-        for (int num : nums) {
-            if (candidate1 == num) {
+        int cand1 = 0; 
+        int cand2 = 0;
+        int count1 = 0;
+        int count2 = 0;
+        for(int i=0; i <nums.length; i++){
+            if(cand1 == nums[i])
+            count1++;
+            else if(cand2 == nums[i])
+            count2++;
+            else if(count1 == 0){
+                cand1 = nums[i];
                 count1++;
-            } else if (candidate2 == num) {
+            }
+            else if(count2 == 0){
+                cand2 = nums[i];
                 count2++;
-            } else if (count1 == 0) {
-                candidate1 = num;
-                count1 = 1;
-            } else if (count2 == 0) {
-                candidate2 = num;
-                count2 = 1;
-            } else {
+            }
+            else {
                 count1--;
                 count2--;
             }
+            
         }
-
-        // Verify candidates
-        count1 = 0;
-        count2 = 0;
-
-        for (int num : nums) {
-            if (num == candidate1)
-                count1++;
-            else if (num == candidate2)
-                count2++;
+        int c1 = 0;
+        int c2 = 0;
+        for(int  i=0;i<nums.length;i++){
+            if(cand1 == nums[i])
+            c1++;
+            else if(cand2 == nums[i])
+            c2++;
         }
-
         List<Integer> ans = new ArrayList<>();
-
-        if (count1 > nums.length / 3)
-            ans.add(candidate1);
-
-        if (count2 > nums.length / 3)
-            ans.add(candidate2);
+        int limit = (int)Math.floor(nums.length/3.0);
+        if(c1>limit)
+        ans.add(cand1);
+        if(c2>limit)
+        ans.add(cand2);
 
         return ans;
     }
