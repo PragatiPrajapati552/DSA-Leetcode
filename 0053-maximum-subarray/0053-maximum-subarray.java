906576368b1a921ed238cc078243866574bf9a8a
PRAGATI PRAJAPATI <pragatiprajapati552@gmail.com>
2026-07-18 02:58:29 +0530

    Time: 1 ms (99.96%), Space: 80.6 MB (7.86%) - LeetHub

---
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        // int s = nums[0];
+        int sum = nums[0];
+        int max = nums[0];
+        for(int i=1; i<nums.length;i++){    
+            if(sum+nums[i] >= nums[i]){
+            sum += nums[i];    
+            }
+            else{
+            sum = nums[i];    
+            }
+            max = Math.max(max,sum); 
+        }
+        return max;
+    }
+}
