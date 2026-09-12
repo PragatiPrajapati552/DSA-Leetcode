bb050238c32898f229a24fdde3aac630a15eae81
PRAGATI PRAJAPATI <pragatiprajapati552@gmail.com>
2026-09-13 02:08:16 +0530

    Time: 0 ms (100%), Space: 43.6 MB (50.78%) - LeetHub

--- 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
+++ 0199-binary-tree-right-side-view/0199-binary-tree-right-side-view.java
@@ -1,52 +1,74 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-     static class Info{
-        int vd;
-        TreeNode node;
-        Info(int vd, TreeNode node){
-            this.vd = vd;
-            this.node = node;
-        }
-    }
-    public List<Integer> rightSideView(TreeNode root) {
-        Queue<Info> q = new LinkedList<>();
-        HashMap<Integer, TreeNode> map = new HashMap<>();
-        List<Integer> list = new ArrayList<>();
-        int max = 0;
-        if(root == null)
-        return list;
-        q.add(new Info(0, root));
-        while(!q.isEmpty()){
-            Info curr = q.remove();
-            // if(!map.containsKey(curr.hd))
-            map.put(curr.vd, curr.node);
+// /**
+//  * Definition for a binary tree node.
+//  * public class TreeNode {
+//  *     int val;
+//  *     TreeNode left;
+//  *     TreeNode right;
+//  *     TreeNode() {}
+//  *     TreeNode(int val) { this.val = val; }
+//  *     TreeNode(int val, TreeNode left, TreeNode right) {
+//  *         this.val = val;
+//  *         this.left = left;
+//  *         this.right = right;
+//  *     }
+//  * }
+//  */
+// class Solution {
+//      static class Info{
+//         int vd;
+//         TreeNode node;
+//         Info(int vd, TreeNode node){
+//             this.vd = vd;
+//             this.node = node;
+//         }
+//     }
+//     public List<Integer> rightSideView(TreeNode root) {
+//         Queue<Info> q = new LinkedList<>();
+//         HashMap<Integer, TreeNode> map = new HashMap<>();
+//         List<Integer> list = new ArrayList<>();
+//         int max = 0;
+//         if(root == null)
+//         return list;
+//         q.add(new Info(0, root));
+//         while(!q.isEmpty()){
+//             Info curr = q.remove();
+//             // if(!map.containsKey(curr.hd))
+//             map.put(curr.vd, curr.node);
 
-            if(curr.node.left != null){
-                q.add(new Info(curr.vd+1, curr.node.left));
-                max = Math.max(curr.vd +1 , max);
-            }
-            if(curr.node.right != null){
-                q.add(new Info(curr.vd+1, curr.node.right));
-                max = Math.max(curr.vd +1 , max);
-            }
+//             if(curr.node.left != null){
+//                 q.add(new Info(curr.vd+1, curr.node.left));
+//                 max = Math.max(curr.vd +1 , max);
+//             }
+//             if(curr.node.right != null){
+//                 q.add(new Info(curr.vd+1, curr.node.right));
+//                 max = Math.max(curr.vd +1 , max);
+//             }
+//         }
+//         for(int i = 0;i<=max;i++){
+//             list.add(map.get(i).val);
+//         }
+//         return list;
+//     }
+// }
+
+//More easy recurrsive solution
+class Solution {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+
+        view(root, 0, ans);
+
+        return ans;
+    }
+
+    private void view(TreeNode root, int level, List<Integer> ans) {
+        if (root == null) return;
+
+        if (level == ans.size()) {
+            ans.add(root.val);
         }
-        for(int i = 0;i<=max;i++){
-            list.add(map.get(i).val);
-        }
-        return list;
+
+        view(root.right, level + 1, ans);
+        view(root.left, level + 1, ans);
     }
 }
